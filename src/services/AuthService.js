908a82c990ff203be1481d80606afb29d52e643e
vishnuxx <vishnuvs2002it@gmail.com>
2023-05-26 19:30:42 +0530

    setup firebase

---
+++ src/services/AuthService.js
@@ -0,0 +1,9 @@
+function AuthService() {
+  this.signup = (username, email, password, confirmpassword) => {};
+
+  this.login = (email , password) => {}
+
+  this.forgotPassword = (email) => {}
+}
+
+export default AuthService;

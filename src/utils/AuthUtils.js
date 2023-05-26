160da5dd965c09c54f9b0425697956696eebd09e
vishnuxx <vishnuvs2002it@gmail.com>
2023-05-26 23:39:57 +0530

    replaced AuthUtils/matchPasswords function to validateSignupInput()

--- src/utils/AuthUtils.js
+++ src/utils/AuthUtils.js
@@ -1,19 +1,46 @@
 import { REACT_APP_FIREBASE_TOKEN_KEY } from "../constants/Envs";
 
+export function validateSignupInput(username, email, password , confirmpassword) {
+  const errors = {};
 
-export function matchPasswords(password, confirmPassword) {
+  // Validate username
+  if (!username || username.trim() === "") {
+    errors.username = "Username is required";
+  }
+
+  // Validate email
+  if (!email || email.trim() === "") {
+    errors.email = "Email is required";
+  } else if (!isValidEmail(email)) {
+    errors.email = "Invalid email format";
+  }
+
+  // Validate password
+  if (!password || password.trim() === "") {
+    errors.password = "Password is required";
+  } else if (password.length < 6) {
+    errors.password = "Password should be at least 6 characters long";
+  }
+
+  // Validate confirmpassword
+  if (!confirmpassword || confirmpassword.trim() === "") {
+    errors.confirmpassword = "Confirmpassword is required";
+  }
+
   // Check if the passwords are the same.
-  if (password !== confirmPassword) {
-    return false;
+  if (password !== confirmpassword) {
+    errors.passwordmatch = "Passwords doesnt match"
   }
 
-  // Check if the password is at least 6 characters long.
-  if (password.length < 6) {
-    return false;
-  }
-
-  // The passwords match and are at least 6 characters long.
+  if(Object.keys(errors).length > 0) throw new Error(errors)
+  
   return true;
+}
+
+export function isValidEmail(email) {
+  // Simple email format validation
+  const emailRegex = /^\S+@\S+\.\S+$/;
+  return emailRegex.test(email);
 }
 
 

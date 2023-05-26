72562cd3dc003473b8d84134c787dd9a46298609
vishnuxx <vishnuvs2002it@gmail.com>
2023-05-26 14:48:10 +0530

    project cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import { MediapipeScreen } from './Components/MediapipeScreen';
+
+import './styles/App.css';
+
+
 
 
 function App() {
   return <div className="App">
-    <MediapipeScreen></MediapipeScreen>
+    
     </div>;
 }
 

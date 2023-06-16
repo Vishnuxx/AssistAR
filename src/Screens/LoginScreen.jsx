3c3c21b39838f4ee9e5c1585c1e319a402fab6b6
vishnuxx <vishnuvs2002it@gmail.com>
2023-06-16 22:27:15 +0530

    implemented login handler

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -1,30 +1,66 @@
 import { Link } from "react-router-dom";
 import style from "../styles/screens/login.module.css"
 import bgimage from "../assets/img2.png"
+import { useState } from "react";
+import { Authenticator } from "../services/AuthService";
 
 function LoginScreen() {
+  
+   const [email, setemail] = useState("");
+   const [password, setpassword] = useState("");
+   const [errormessage, seterrormessage] = useState("");
+
+   const handleLogin = async () => {
+     try {
+      console.log(email , password)
+       const response = await Authenticator.login(
+         email,
+         password,
+       );
+       seterrormessage("");
+       console.log(response);
+     } catch (error) {
+       seterrormessage(error.toString());
+     }
+   };
+
   return (
     <>
       <div className={style.center}>
         <h1>Login</h1>
-        <form method={style.post}>
+        <form>
           <div className={style.txt_field}>
-            <label>Username/Email</label>
+            <label>Email</label>
             <br />
             <br />
-            <input type="text" required />
+            <input
+              type="text"
+              onChange={(e) => setemail(e.target.value)}
+              required
+            />
           </div>
 
           <div className={style.txt_field}>
             <label>Password</label>
             <br />
             <br />
-            <input type={style.password} required />
+            <input
+              className={style.password}
+              type="password"
+              onChange={(e) => setpassword(e.target.value)}
+              required
+            />
 
             <br />
             <br />
           </div>
-          <input type="submit" value="Login" />
+          <button
+            onClick={handleLogin}
+            type="button"
+            className={style.submitbtn}
+          >
+            Submit
+          </button>
           <br />
           <br />
           <div className={style.signup_link}>
@@ -33,13 +69,14 @@ function LoginScreen() {
             <br />
           </div>
           <div className={style.pass}>
-            <Link >Forgot password?</Link>
+            <Link>Forgot password?</Link>
           </div>
+          <p>{errormessage}</p>
         </form>
       </div>
       <div className={style.img}>
-       <img alt="" src={bgimage} width="240px" height="240px" /> 
-    </div>
+        <img alt="" src={bgimage} width="240px" height="240px" />
+      </div>
     </>
   );
 }

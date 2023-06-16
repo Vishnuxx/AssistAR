e7c7d638d1f5e52b0a843ca862a9d57eda6184d8
vishnuxx <vishnuvs2002it@gmail.com>
2023-06-16 22:39:06 +0530

    minor change

--- src/Screens/SignUpScreen.jsx
+++ src/Screens/SignUpScreen.jsx
@@ -1,6 +1,6 @@
 import style from "../styles/screens/signup.module.css";
 import bgimage from "../assets/img2.png";
-import { Authenticator } from "../services/AuthService";
+import { auth_service } from "../services/AuthService";
 import { useState } from "react";
 import { validateSignupInput } from "../utils/AuthUtils";
 
@@ -13,19 +13,16 @@ function SignUpScreen() {
 
   const handleSignup = async () => {
     try {
-
-      const match = validateSignupInput(
+      const response = await auth_service.signup(
         username,
         email,
         password,
         confirmPassword
       );
-      console.log(match)
-    //   if (match) {
-    //     await Authenticator.signup(username, email, password, confirmPassword);
-    //   }
+      console.log(response);
+      seterrormessage("");
     } catch (error) {
-      seterrormessage(error.toString())
+      seterrormessage(error.toString());
     }
   };
   return (
@@ -33,7 +30,7 @@ function SignUpScreen() {
       <div className={style.center}>
         <h1>Sign Up</h1>
         <br />
-        <form>
+        <div>
           <div className={style.txt_field}>
             <label>Username</label>
             <br />
@@ -90,12 +87,14 @@ function SignUpScreen() {
             onClick={handleSignup}
             type="button"
             className={style.submitbtn}
-          >Submit</button>
+          >
+            Submit
+          </button>
           <div className={style.login_link}>
             Already have an account? <a href="MRAmob2.html">Login</a>
           </div>
           <p>{errormessage}</p>
-        </form>
+        </div>
       </div>
       <div className={style.img}>
         <img alt="" src={bgimage} width="240px" height="240px" />
